3ac1b574b3cec94e43aa71181e4c7fa4dd82f47a
ashah918 <70021049+ashah918@users.noreply.github.com>
2020-11-20 21:04:57 +0800

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,7 +1,12 @@
-//gcd.h
-#ifndef deck.h
-#define deck.h
+//Deck.h
+#ifndef DECK_H
+#define DECK_H
 
-void build_deck(string *deck);
+void build_deck(std::string *deck);
+void shuffle_deck(std::string *deck);
+std::string deal_cards(std::string *deck);
+void add_to_player_deck(std::string *player_deck, int &p_counter, std::string *deck);
+void add_to_comp_deck(std::string *comp_deck, int &c_counter, std::string *deck);
+int player_sum(std::string *player_deck, int &p_counter);
 
 #endif
