c6bd93127d29af35cecda9cd8a57824f9860fe87
ashah918 <70021049+ashah918@users.noreply.github.com>
2020-11-20 21:29:34 +0800

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,4 +1,4 @@
-//Deck.h
+//deck.h
 #ifndef DECK_H
 #define DECK_H
 
@@ -8,5 +8,7 @@ std::string deal_cards(std::string *deck);
 void add_to_player_deck(std::string *player_deck, int &p_counter, std::string *deck);
 void add_to_comp_deck(std::string *comp_deck, int &c_counter, std::string *deck);
 int player_sum(std::string *player_deck, int &p_counter);
+int comp_sum(string *comp_deck, int &c_counter);
+void determine_w_l(int p_hand_value, int c_hand_value);
 
 #endif
