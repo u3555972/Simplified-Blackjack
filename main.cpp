389eed0c29a5921202e6102c72766e81d7c54f33
ashah918 <70021049+ashah918@users.noreply.github.com>
2020-11-20 21:48:46 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,16 +24,18 @@ void displaycurrent(string name){
 
 void newg(string name){
   //new game opens deletes originally saved game and gives computer $10000
-  ifstream fin;
-  fin.open(name);
   char n[50];
   for (int i=0; i<name.length(); ++i){
     n[i]=name[i];
   }
+  n[name.length()]='.';
+  n[name.length()+1]='t';
+  n[name.length()+2]='x';
+  n[name.length()+3]='t';
   remove(n);
   ofstream fout;
-  fout.open(name);
-  fout<<name<<'\n'<<"10000"<<'\n'<<'\n'<<"10000"<<'\n'<<endl;
+  fout.open(name+".txt");
+  fout<<name<<'\n'<<"10000"<<'\n'<<"10000"<<'\n'<<endl;
   fout.close();
   displaycurrent(name);
 }
@@ -56,13 +58,13 @@ int main() {
   cin>>name;
   //check if file exists
   ifstream fin;
-  fin.open(name);
+  fin.open(name+".txt");
   //if it doesnt, create a new file
   if (fin.fail()){
     newg(name);
   }
   else {
-    cout<<"You have a saved game. Do you want to continue (C) or start a new game (N): ";
+    cout<<"You have a saved game. Do you want to continue (C) or overwrite and start a new game (N): ";
     cin>>saved;
     if (saved=='C'){
       displaycurrent(name);
@@ -76,223 +78,4 @@ int main() {
     }
   }
   fin.close();
-
-  char newgame = 'Y';
-  while (newgame == 'Y') {
-    string *deck = new string[52];
-    string *player_deck = new string[5];
-    string *comp_deck = new string [5];
-    int p_counter = 0;
-    int c_counter = 0;
-
-    build_deck(deck);
-    shuffle_deck(deck);
-
-    cout << "Your Hand:" << endl;
-    add_to_player_deck(player_deck, p_counter, deck);
-    add_to_player_deck(player_deck, p_counter, deck);
-    int p = 0;
-    while (p < p_counter) {
-      cout << player_deck[p];
-      if (p < (p_counter-1)) {
-        cout << " ";
-      }
-      p += 1;
-    }
-    int p_hand_value = player_sum(player_deck, p_counter);
-    cout << endl;
-    cout << "Hand Value: " << p_hand_value << endl;
-    cout << endl;
-
-    cout << "Dealer draws 2 cards..." << endl;
-    add_to_comp_deck(comp_deck, c_counter, deck);
-    add_to_comp_deck(comp_deck, c_counter, deck);
-    // int c = 0;
-    // while (c < c_counter) {
-    //   cout << comp_deck[c];
-    //   if (c < (c_counter-1)) {
-    //     cout << " ";
-    //   }
-    //   c+= 1;
-    // }
-    // cout << endl;
-    int c_hand_value = comp_sum(comp_deck, c_counter);
-    cout << endl;
-
-    bool skip = true, busted = false;
-    if (p_hand_value > 21) {
-      cout << "Busted! You lose..." << endl;
-    }
-
-    else if (p_hand_value == 21) {
-      cout << "Blackjack! You win!" << endl;
-    }
-
-    else if (c_hand_value == 21) {
-      cout << "Dealer has a Blackjack! You lose..." << endl;
-    }
-
-    else if (c_hand_value > 21) {
-      cout << "Dealer busted! You win!" << endl;
-    }
-
-    else {
-      char decision = 'Y';
-      while (decision == 'Y') {
-        cout << "Do you want to draw a card (hit) (Y/N)?: ";
-        cin >> decision;
-        if (decision == 'Y') {
-          add_to_player_deck(player_deck, p_counter, deck);
-          int p = 0;
-          while (p < p_counter) {
-            cout << player_deck[p];
-            if (p < (p_counter-1)) {
-              cout << " ";
-            }
-            p += 1;
-          }
-          int p_hand_value = player_sum(player_deck, p_counter);
-          cout << endl;
-          cout << "Hand Value: " << p_hand_value << endl;
-          //cout << p_hand_value << endl;
-          if (p_hand_value > 21) {
-            cout << "Busted! You lose..." << endl;
-            decision = 'N';
-            skip = false;
-            busted = true;
-          }
-        }
-      }
-    }
-    if (skip) {
-      if (c_hand_value < 17) {
-        cout << "Dealer draws a card..." << endl;
-        add_to_comp_deck(comp_deck, c_counter, deck);
-      }
-      p_hand_value = player_sum(player_deck, p_counter);
-      cout << endl;
-      cout << "Dealer's Hand:" << endl;
-      int c = 0;
-      while (c < c_counter) {
-        cout << comp_deck[c];
-        if (c < (c_counter-1)) {
-          cout << " ";
-        }
-        c+= 1;
-      }
-      cout << endl;
-      c_hand_value = comp_sum(comp_deck, c_counter);
-      cout << "Dealer's Hand Value: " << c_hand_value << endl;
-      cout << endl;
-      determine_w_l(p_hand_value, c_hand_value);
-      cout << endl;
-    }
-
-    else if (busted == true) {
-      cout << endl;
-      cout << "Dealer's Hand:" << endl;
-      int c = 0;
-      while (c < c_counter) {
-        cout << comp_deck[c];
-        if (c < (c_counter-1)) {
-          cout << " ";
-        }
-        c+= 1;
-      }
-      cout << endl;
-      c_hand_value = comp_sum(comp_deck, c_counter);
-      cout << "Dealer's Hand Value: " << c_hand_value << endl;
-      cout << endl;
-    }
-
-    else {
-      if (c_hand_value < 17) {
-        cout << "Dealer draws a card..." << endl;
-        add_to_comp_deck(comp_deck, c_counter, deck);
-      }
-      //p_hand_value = player_sum(player_deck, p_counter);
-      cout << endl;
-      cout << "Dealer's Hand:" << endl;
-      int c = 0;
-      while (c < c_counter) {
-        cout << comp_deck[c];
-        if (c < (c_counter-1)) {
-          cout << " ";
-        }
-        c+= 1;
-      }
-      cout << endl;
-      c_hand_value = comp_sum(comp_deck, c_counter);
-      cout << "Dealer's Hand Value: " << c_hand_value << endl;
-      cout << endl;
-    }
-    cout << "Do you want to play again (Y/N)?: ";
-    cin >> newgame;
-  }
-  cout << endl;
-  cout << "Thank you for playing!" << endl;
 }
-
-  // int c = 0;
-  // while (c < c_counter) {
-  //   cout << comp_deck[c];
-  //   if (c < (c_counter-1)) {
-  //     cout << " ";
-  //   }
-  //   c+= 1;
-  // }
-  // cout << endl;
-  // c_hand_value = comp_sum(comp_deck, c_counter);
-  // cout << "Dealer's Hand Value: " << c_hand_value;
-
-
-
-
-  // for (int card=0; card<52; card++) {
-  //   cout<< deck[card] << endl;
-  // }
-
-  // int p_counter = 0;
-  // for (int p_cards=0; p_cards<2; p_cards++) {
-  //   string t = deal_cards(deck);
-  //   player_deck[p_cards] = t;
-  //   p_counter ++;
-  // }
-
-  // int c_counter = 0;
-  // for (int c_cards=0; c_cards<2; c_cards++) {
-  //   string t = deal_cards(deck);
-  //   comp_deck[c_cards] = t;
-  //   c_counter ++;
-  // }
-
-  //string *d_ptr = build_deck(deck);
-  // for (int k=0; k<52; k++) {
-  //   cout << d_ptr[k] << endl;
-  // }
-
-  //string *ds_ptr = shuffle_deck(d_ptr);
-  // for (int x=0; x <52; x++) {
-  //   cout << ds_ptr[x] << endl;
-  // }
-
-  // string t = deal_cards(ds_ptr);
-  // cout << t;
-  //
-  // for (int y=0; y<51; y++) {
-  //   cout << deck[y] << endl;
-  // }
-
-//   string deck[52];
-//   int counter = 0;
-//   string rank[13] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-//   string suit[4] = {"\u2660", "\u2661", "\u2662", "\u2663"};
-//   for (int i=0; i<13; i++) {
-//     for (int j=0; j<4; j++) {
-//       deck[counter] = rank[i] + suit[j];
-//       counter ++;
-//     }
-//   }
-//   for (int k=0; k<52; k++) {
-//     cout << deck[k] << endl;
-//   }
