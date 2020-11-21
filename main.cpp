be6988b4564a1d9cbd84a823bf2c9cb1f5e33da5
u3555972 <71867489+u3555972@users.noreply.github.com>
2020-11-21 14:57:04 +0800

    Updated instructions

--- main.cpp
+++ main.cpp
@@ -47,16 +47,21 @@ int main() {
   char saved;
   cout<<"Welcome to Blackjack!"<<endl;
   cout<<"Instructions:"<<endl;
-  cout<<"The aim of the game is to get a total as close to the number 21 (inclusively) as possible. Two regular cards will be dealt to the player and the computer. The cards 2-10 are worth their own numbers while \'J\', \'Q\' and \'K\' are worth 10 each. Furthermore, \'A\' can either be worth 1 or 11 points depending on the users choice. At the beginning of each round, the player can choose to either ask for 1 more card to increase their total points or they can choose to stay, keeping their hand. The player with the closest total points to 21 or has the sum of 21 with the lowest number of cards wins! Unfortunately, if the player's total does surpass 21, they are \"busted\" (loses the match)"<<endl;
+  cout<<"The aim of the game is to get a hand value of 21 or as closest as possible without exceeding it." <<endl;
+  cout<<"J, Q, K, are worth 10, while the numbers are worth their own number (e.g. 10 = 10) " << endl;
+  
   cout<<endl;
+  cout<<"--------------------------------------------------------------------------------"<<endl;
   cout<<"1) Each player starts with $10,000"<<endl;
   cout<<"2) The minimum betting for each player is $1,000 per match"<<endl;
-  cout<<"3) After recieving the player recieves their cards, they may choose to raise their betting amount"<<endl;
-  cout<<"4) After a player raises, the player must match the raise and the round commences. If the player is unable to match the raise, they lose the round immediately"<<endl;
-  cout<<"5) If a round is matched, players can either request another card, reveal their cards or fold (forfeit match)"<<endl;
-  cout<<"6) If a player cannot pay the minimum bet or loses all their money at the end of a round, they lose the match and the game ends."<<endl;
+  cout<<"3) After recieving the player receives their cards, they may choose to raise their betting amount"<<endl;
+  cout<<"4) After a player raises, the dealer must match the raise and the round commences. If the dealer is unable to match the raise, they lose the round immediately"<<endl;
+  cout<<"5) If a round is matched, players can either request another card or reveal their cards"<<endl;
+  cout<<"6) If a player cannot pay the minimum bet, they must go all in (use all money)" << endl;
+  cout<<"7) If the player loses money at the end of a round, they lose the game!"<<endl;
   cout<<"--------------------------------------------------------------------------------"<<endl;
-  cout<<"What is your name? ";
+  cout<<endl;
+  cout<<"What is your name?: ";
   cin>>name;
   //check if file exists
   ifstream fin;
