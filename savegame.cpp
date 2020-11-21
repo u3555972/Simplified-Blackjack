7dfa7b67cc2c9c05c6497a1d5f013e4cebad8181
ashah918 <70021049+ashah918@users.noreply.github.com>
2020-11-21 15:26:53 +0800

    comments

--- savegame.cpp
+++ savegame.cpp
@@ -9,6 +9,8 @@ using namespace std;
 void displaycurrent(string name){
   string moneyp, moneyc;
   //prints name and total money of player and computer
+  //input of the string name such that name.txt can be opened
+  //output name, amount of money player has and amount of money dealer has
   ifstream fin;
   fin.open(name+".txt");
   cout<<"Name: ";
@@ -26,6 +28,8 @@ void displaycurrent(string name){
 
 void newg(string name){
   //new game opens deletes originally saved game and gives computer $10000
+  //input of name such that the origianlly saved file of name.txt is deleted and a new one is created.
+  //inside the file is name, and $10000 to each player
   char n[50];
   for (int i=0; i<name.length(); ++i){
     n[i]=name[i];
